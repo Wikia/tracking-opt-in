e119355e406fcff2c9f8ff15b5ed04b3b6a59f57
drsnyder <drsnyder@gmail.com>
2018-05-07 13:08:34 -0500

    Add karma config

---
+++ karma.conf.js
@@ -0,0 +1,90 @@
+// Karma configuration
+// Generated on Mon May 07 2018 11:09:08 GMT-0500 (CDT)
+
+module.exports = function(config) {
+  config.set({
+      // base path that will be used to resolve all patterns (eg. files, exclude)
+      basePath: '',
+
+      // frameworks to use
+      // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+      frameworks: ['mocha'],
+
+      // list of files / patterns to load in the browser
+      files: [
+          'src/**/*.js'
+      ],
+
+      // list of files / patterns to exclude
+      exclude: [
+      ],
+
+      webpack: {
+          mode: 'production',
+          module: {
+              rules: [
+                  {
+                      test: /\.js$/,
+                      use: [{
+                          loader: 'babel-loader',
+                          options: {
+                              presets: ['env', 'preact'],
+                              plugins: [
+                                  'transform-class-properties',
+                              ]
+                          }
+                      }]
+                  },
+                  {
+                      test: /\.scss$/,
+                      use: [{
+                          loader: 'null-loader',
+                      }],
+                  }
+              ],
+          },
+      },
+
+      plugins: [
+          'karma-mocha',
+          'karma-webpack',
+          'karma-jsdom-launcher',
+      ],
+
+      // preprocess matching files before serving them to the browser
+      // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+      preprocessors: {
+          'src/**/*.js': ['webpack']
+      },
+
+      // test results reporter to use
+      // possible values: 'dots', 'progress'
+      // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+      reporters: ['progress'],
+
+      // web server port
+      port: 9876,
+
+      // enable / disable colors in the output (reporters and logs)
+      colors: true,
+
+      // level of logging
+      // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+      logLevel: config.LOG_INFO,
+
+      // start these browsers
+      // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+      browsers: ['jsdom'],
+
+      // Continuous Integration mode
+      // if true, Karma captures browsers, runs the tests and exits
+      singleRun: true,
+
+      // enable / disable watching file and executing tests whenever any file changes
+      autoWatch: true,
+
+      // Concurrency level
+      // how many browser should be started simultaneous
+      concurrency: 1,
+  })
+};
