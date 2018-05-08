64a967d593ae85212ee478b066760d3a95cc1f0f
drsnyder <drsnyder@gmail.com>
2018-05-08 17:34:57 -0500

    Add code coverage reporting

--- karma.conf.js
+++ karma.conf.js
@@ -70,19 +70,25 @@ module.exports = function(config) {
       plugins: [
           'karma-mocha',
           'karma-webpack',
+          'karma-coverage',
           'karma-jsdom-launcher',
       ],
 
       // preprocess matching files before serving them to the browser
       // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
       preprocessors: {
-          'src/**/*.js': ['webpack']
+          'src/**/*.js': ['webpack', 'coverage']
       },
 
       // test results reporter to use
       // possible values: 'dots', 'progress'
       // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-      reporters: ['progress'],
+      reporters: ['progress', 'coverage'],
+
+      coverageReporter: {
+          type : 'html',
+          dir : 'coverage/'
+      },
 
       // web server port
       port: 9876,
