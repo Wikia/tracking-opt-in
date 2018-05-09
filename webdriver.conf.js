b07bf0077c00cc6f84f181ff849e39b0ae306d09
nmonterroso <nmonterroso@users.noreply.github.com>
2018-05-09 11:13:36 -0700

    more plumbing

--- webdriver.conf.js
+++ webdriver.conf.js
@@ -2,28 +2,57 @@ const browserstack = require('browserstack-local');
 
 const user = process.env.BROWSERSTACK_USERNAME;
 const key = process.env.BROWSERSTACK_KEY;
-const local = !!process.env.BROWSERSTACK_LOCAL;
 
 if (!user || !key) {
     console.error('Please provide BROWSERSTACK_USERNAME and BROWSERSTACK_KEY environment params');
     process.exit(1);
 }
 
+const SUITES = {
+    local: 'local',
+    sample: 'sample',
+};
+
+const suite = process.env.SUITE || 'local';
+if (!SUITES[suite]) {
+    console.error('unknown suite', suite);
+    process.exit(1);
+}
+
+const local = SUITES[suite] === SUITES.local;
+const commonCapabilities = {
+    os: 'OS X',
+    os_version: 'High Sierra',
+    project: 'tracking-opt-in',
+    'browserstack.local': local,
+};
+
 exports.config = {
     user,
     key,
     coloredLogs: true,
     specs: [
-        './selenium/sample.js'
+        `./selenium/${SUITES[suite]}.js`
     ],
-    capabilities: [{
-        project: 'tracking-opt-in',
-        browser: 'chrome',
-        'browserstack.local': local,
-    }],
+    capabilities: [
+        {
+            ...commonCapabilities,
+            browser: 'chrome',
+        },
+        // {
+        //     ...commonCapabilities,
+        //     browser: 'firefox',
+        // },
+        // {
+        //     ...commonCapabilities,
+        //     browser: 'safari',
+        // },
+    ],
+};
 
+if (local) {
     // Code to start browserstack local before start of test
-    onPrepare: function (config, capabilities) {
+    exports.config.onPrepare = function(config, capabilities) {
         console.log("Connecting local");
         return new Promise(function(resolve, reject){
             exports.bs_local = new browserstack.Local();
@@ -34,10 +63,10 @@ exports.config = {
                 resolve();
             });
         });
-    },
+    };
 
     // Code to stop browserstack local after end of test
-    onComplete: function (capabilties, specs) {
+    exports.config.onComplete = function (capabilties, specs) {
         exports.bs_local.stop(function() {});
-    }
-};
+    };
+}
