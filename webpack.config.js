2eab1ff95fa32c327c7053e3c86358a76cee99c5
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2018-05-09 14:50:27 -0700

    Always add styles to the script; cleanup webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -15,40 +15,12 @@ const autoprefixerPlugin = autoprefixer({
     cascade: false,
     browsers: browsers.join(', '),
 });
+const isDevevelopment = process.env.NODE_ENV === 'development';
 
-let entry = './src/index.js';
 let topLevelOptions = {};
 let plugins = [];
-let cssLoader = {
-    test: /\.s?css$/,
-    use: [
-        {
-            loader: 'css-loader',
-            options: {
-                sourceMap: false,
-                modules: true,
-            },
-        },
-        {
-            loader: 'postcss-loader',
-            options: {
-                sourceMap: false,
-                plugins: () => [
-                    autoprefixerPlugin,
-                ],
-            },
-        },
-        {
-            loader: 'sass-loader',
-            options: {
-                sourceMap: false
-            },
-        }
-    ],
-};
 
 if (process.env.NODE_ENV === 'development') {
-    entry = './src/index-dev.js';
     topLevelOptions = {
         serve: {
             host: 'localhost',
@@ -66,29 +38,11 @@ if (process.env.NODE_ENV === 'development') {
             to: `${buildPath}/index.html`,
         }])
     ];
-
-    cssLoader.use[0].options.sourceMap = true;
-    cssLoader.use[1].options.sourceMap = true;
-    cssLoader.use[2].options.sourceMap = true;
-    cssLoader.use.unshift({
-        loader: 'style-loader',
-        options: {
-            sourceMap: true,
-        }
-    });
-} else {
-    cssLoader.use.unshift(MiniCssExtractPlugin.loader);
-    plugins = [
-        ...plugins,
-        new MiniCssExtractPlugin({
-            filename: 'styles.css',
-        })
-    ]
 }
 
 module.exports = {
     mode: process.env.NODE_ENV,
-    entry,
+    entry: isDevevelopment ? './src/index.js' : './src/index-dev.js',
     output: {
         path: buildPath,
         filename: 'tracking-opt-in.js',
@@ -102,7 +56,39 @@ module.exports = {
                 include: `${__dirname}/src`,
                 use: 'babel-loader',
             },
-            cssLoader,
+            {
+                test: /\.s?css$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            sourceMap: isDevevelopment,
+                        },
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: isDevevelopment,
+                            modules: true,
+                        },
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: isDevevelopment,
+                            plugins: () => [
+                                autoprefixerPlugin,
+                            ],
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: isDevevelopment
+                        },
+                    }
+                ],
+            },
         ],
     },
     plugins: [
