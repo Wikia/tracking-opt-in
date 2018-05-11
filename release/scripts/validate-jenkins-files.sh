110b9a41a36ee06dc8445cba4871a2cfb6e070ca
drsnyder <drsnyder@gmail.com>
2018-05-11 10:00:18 -0500

    Use and env var for the jenkins url

--- release/scripts/validate-jenkins-files.sh
+++ release/scripts/validate-jenkins-files.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-curl -X POST  -F "jenkinsfile=<release/jenkins" http://jenkins.wikia-prod:8080/pipeline-model-converter/validate
+if [[ -z $JENKINS_URL ]]; then
+    echo "Unable to validate with a \$JENKINS_URL environment variable."
+    exit 1
+fi
+
+curl -X POST  -F "jenkinsfile=<release/jenkins" $JENKINS_URL/pipeline-model-converter/validate
