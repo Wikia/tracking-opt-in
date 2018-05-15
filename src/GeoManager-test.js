f58da20ec7c0060aab97a1903a8c1d61fd137c26
nmonterroso <nmonterroso@users.noreply.github.com>
2018-05-15 15:24:35 -0700

    filling out LangManager tests

--- src/GeoManager-test.js
+++ src/GeoManager-test.js
@@ -13,46 +13,48 @@ function setGeoCookie(country) {
     });
 }
 
-describe('with provided options', () => {
-    it('indicates consent is required', () => {
-        const geoManager = new GeoManager(countryRequiringPrompt, [countryRequiringPrompt]);
-        assert.isOk(geoManager.needsTrackingPrompt());
-    });
-
-    it('indicates consent is not required', () => {
-        const geoManager = new GeoManager(countryNotRequiringPrompt, [countryRequiringPrompt]);
-        assert.isNotOk(geoManager.needsTrackingPrompt());
-    });
-});
-
-describe('with default options', () => {
-    after(() => {
-        Cookies.remove(COUNTRY_COOKIE_NAME);
-    });
-
-    describe('and geo cookie present', () => {
+describe('GeoManager', () => {
+    describe('with provided options', () => {
         it('indicates consent is required', () => {
-            setGeoCookie(countryRequiringPrompt);
-            assert.isOk(new GeoManager().needsTrackingPrompt());
+            const geoManager = new GeoManager(countryRequiringPrompt, [countryRequiringPrompt]);
+            assert.isOk(geoManager.needsTrackingPrompt());
         });
 
         it('indicates consent is not required', () => {
-            setGeoCookie(countryNotRequiringPrompt);
-            assert.isNotOk(new GeoManager().needsTrackingPrompt());
+            const geoManager = new GeoManager(countryNotRequiringPrompt, [countryRequiringPrompt]);
+            assert.isNotOk(geoManager.needsTrackingPrompt());
         });
     });
 
-    describe('and unparseable geo cookie', () => {
-        it('indicates consent is required', () => {
-            Cookies.set(COUNTRY_COOKIE_NAME, '{');
-            assert.isOk(new GeoManager().needsTrackingPrompt());
-        });
-    });
-
-    describe('and no geo cookie', () => {
-        it('indicates consent is required', () => {
+    describe('with default options', () => {
+        after(() => {
             Cookies.remove(COUNTRY_COOKIE_NAME);
-            assert.isOk(new GeoManager().needsTrackingPrompt());
+        });
+
+        describe('and geo cookie present', () => {
+            it('indicates consent is required', () => {
+                setGeoCookie(countryRequiringPrompt);
+                assert.isOk(new GeoManager().needsTrackingPrompt());
+            });
+
+            it('indicates consent is not required', () => {
+                setGeoCookie(countryNotRequiringPrompt);
+                assert.isNotOk(new GeoManager().needsTrackingPrompt());
+            });
+        });
+
+        describe('and unparseable geo cookie', () => {
+            it('indicates consent is required', () => {
+                Cookies.set(COUNTRY_COOKIE_NAME, '{');
+                assert.isOk(new GeoManager().needsTrackingPrompt());
+            });
+        });
+
+        describe('and no geo cookie', () => {
+            it('indicates consent is required', () => {
+                Cookies.remove(COUNTRY_COOKIE_NAME);
+                assert.isOk(new GeoManager().needsTrackingPrompt());
+            });
         });
     });
 });
