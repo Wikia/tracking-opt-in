da9799c782790546d2769ef13104447143ea1766
nmonterroso <nmonterroso@users.noreply.github.com>
2018-05-16 14:04:58 -0700

    adding ContentManager tests

---
+++ src/ContentManager-test.js
@@ -0,0 +1,16 @@
+import {assert} from 'chai';
+import ContentManager, {langToContent} from './ContentManager';
+
+describe('ContentManager', () => {
+    it('gives content in the specified language', () => {
+        assert.equal(new ContentManager('de').content.headline, langToContent.de.headline);
+    });
+
+    it('falls back to en when the language does not exist', () => {
+        assert.equal(new ContentManager('xx').content.headline, langToContent.en.headline);
+    });
+
+    it('falls back to en when the language is not specified', () => {
+        assert.equal(new ContentManager().content.headline, langToContent.en.headline);
+    });
+});
