78ef2975f1b5d72720bbe47b8375a4263392c4da
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2019-07-29 09:55:06 -0700

    Add test

---
+++ src/components/Switch-test.js
@@ -0,0 +1,48 @@
+import { h, render } from 'preact';
+import { expect, assert } from 'chai';
+import { createStubInstance, stub } from 'sinon';
+import Switch from './Switch';
+import styles from './styles.scss';
+
+const document = global.document;
+
+function findByTag(wrapper, tagName) {
+    return wrapper.getElementsByTagName(tagName).item(0) || new HTMLElement();
+}
+
+function noop() { }
+
+describe('Switch', () => {
+    let wrapper;
+
+    function renderSwitch(callbacks = {}) {
+        return render(h(Switch, {
+            isOn: false,
+            onChange: callbacks.onChange || noop,
+        }), document.body);
+    }
+
+    function removeSwitch() {
+        render(null, document.body, document.body.lastChild);
+    }
+
+    afterEach(() => {
+        removeSwitch();
+    });
+
+    it('renders the modal', () => {
+        const wrapper = renderSwitch();
+        expect(wrapper.className).to.equal(styles.switch);
+    });
+
+    it('calls onChange when checkbox is clicked', () => {
+        const onChange = stub();
+
+        const wrapper = renderSwitch({ onChange });
+        const checkbox = findByTag(wrapper, 'input');
+        expect(checkbox).to.not.equal(null);
+        checkbox.click();
+
+        assert.isOk(onChange.called);
+    });
+});
