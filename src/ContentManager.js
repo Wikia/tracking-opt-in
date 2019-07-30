9a223a55d969d2f7534ee116ae9d70f354d0afe4
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2019-07-30 12:09:07 -0700

    Nuke obsolete file and add comments

--- src/ContentManager.js
+++ src/ContentManager.js
@@ -8,7 +8,6 @@ import pl from './i18n/pl.json';
 import pt from './i18n/pt.json';
 import ru from './i18n/ru.json';
 import zh from './i18n/zh.json';
-import zhHk from './i18n/zh-hk.json';
 import zhTw from './i18n/zh-tw.json';
 
 export const langToContent = {
@@ -21,9 +20,9 @@ export const langToContent = {
     pl: pl,
     pt: pt,
     ru: ru,
-    zh: zh,
-    'zh-hk': zhHk,
-    'zh-tw': zhTw,
+    zh: zh, // simplified
+    'zh-hk': zhTw, // traditional
+    'zh-tw': zhTw, // traditional
 };
 
 export default class ContentManager {
