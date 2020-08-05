3a34144cf4604918394c8d264172ed84efc79bbe
Damian Sobczak <dsobczak@wikia-inc.com>
2020-08-05 13:16:10 +0200

    ADEN-10503 Share ICBM instance to allow postponing GP and Sports execution

--- src/shared/GeoManager.js
+++ src/shared/GeoManager.js
@@ -3,6 +3,7 @@ import { debug, getJSON, getUrlParameter } from './utils';
 
 export const COUNTRY_COOKIE_NAME = 'Geo';
 const MISSING_COOKIE_NAME = 'no-cookie';
+let icbmLoaded = false;
 let icbmContent = null;
 
 // client.geo.country_code https://docs.fastly.com/guides/vcl/geolocation-related-vcl-features
@@ -89,57 +90,60 @@ function getGeoDataFromCookie(type = 'country') {
     return false;
 }
 
-class GeoManager {
-    tcf2Enabled = false;
+function isVariableEnabled(name) {
+    if (getUrlParameter(`icbm.${name}`)) {
+        return getUrlParameter(`icbm.${name}`) === 'true';
+    }
 
+    return !!(
+        icbmContent &&
+        icbmContent[name] &&
+        icbmContent[name][0] &&
+        icbmContent[name][0].value &&
+        icbmContent[name][0].regions &&
+        (
+            icbmContent[name][0].regions.includes(this.country.toUpperCase()) ||
+            icbmContent[name][0].regions.includes('XX')
+        ));
+}
+
+class GeoManager {
     constructor(country, region, countriesRequiringPrompt) {
         this.geosRequiringPrompt = (countriesRequiringPrompt || COUNTRIES_REQUIRING_PROMPT).map(country => country.toLowerCase());
         this.country = (country || getGeoDataFromCookie('country') || MISSING_COOKIE_NAME).toLowerCase();
         this.region = (region || getGeoDataFromCookie('region') || MISSING_COOKIE_NAME).toLowerCase();
 
-        this.fetchInstantConfig().then(() => {
-            debug('GEO', 'ICBM called', icbmContent);
-
-            this.tcf2Enabled = this.isVariableEnabled('icTcf2Enabled');
+        GeoManager.fetchInstantConfig().then(() => {
+            this.tcf2Enabled = isVariableEnabled('icTcf2Enabled');
 
             debug('GEO', `Variables set: tcf2Enabled is ${this.tcf2Enabled}`);
-        }, () => {
-            debug('GEO', 'Failed to call ICBM service');
-
-            this.tcf2Enabled = false;
         });
     }
 
-    fetchInstantConfig() {
-        if (!icbmContent) {
+    static fetchInstantConfig() {
+        if (!icbmLoaded) {
+            if (icbmContent) {
+                return icbmContent;
+            }
+
             // Let's use Oasis as a source of truth and change icVar for all platforms with new library version
-            icbmContent = getJSON('https://services.wikia.com/icbm/api/config?app=oasis').then((content) => {
-                icbmContent = content;
-            }, () => {
-                icbmContent = {};
-            });
+            icbmContent = getJSON('https://services.wikia.com/icbm/api/config?app=oasis')
+                .then((content) => {
+                    debug('GEO', 'ICBM called', content);
+
+                    icbmLoaded = true;
+                    icbmContent = content;
+                }, () => {
+                    debug('GEO', 'Failed to call ICBM service');
+
+                    icbmLoaded = true;
+                    icbmContent = {};
+                });
 
             return icbmContent;
         }
 
-        return icbmContent;
-    }
-
-    isVariableEnabled(name) {
-        if (getUrlParameter(`icbm.${name}`)) {
-            return getUrlParameter(`icbm.${name}`) === 'true';
-        }
-
-        return !!(
-            icbmContent &&
-            icbmContent[name] &&
-            icbmContent[name][0] &&
-            icbmContent[name][0].value &&
-            icbmContent[name][0].regions &&
-            (
-                icbmContent[name][0].regions.includes(this.country.toUpperCase()) ||
-                icbmContent[name][0].regions.includes('XX')
-            ));
+        return Promise.resolve();
     }
 
     needsTrackingPrompt() {
