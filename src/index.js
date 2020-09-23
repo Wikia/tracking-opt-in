f5d196f7e444b442c4d5acdfb90414f254f509df
Damian Sobczak <dsobczak@wikia-inc.com>
2020-09-23 12:42:39 +0200

    ADEN-10592 Switch modal version variable source

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import { IAB_VENDORS, addNonIABVendors } from './shared/consts';
+import { IAB_VENDORS } from './shared/consts';
 import ContentManager from './shared/ContentManager';
 import GeoManager from './shared/GeoManager';
 import LanguageManager from './shared/LangManager';
 import ConsentManagementProvider from './gdpr/ConsentManagementProvider';
-import ConsentManagementProviderLegacy from './gdpr/ConsentManagementProviderLegacy';
 import OptInManager from './gdpr/OptInManager';
 import Tracker from './gdpr/Tracker';
 import TrackingOptIn from './gdpr/TrackingOptIn';
@@ -54,11 +53,6 @@ function initializeGDPR(options) {
     const geoManager = new GeoManager(depOptions.country, depOptions.region, depOptions.countriesRequiringPrompt);
     const tracker = new Tracker(langManager.lang, geoManager.getDetectedGeo(), depOptions.beaconCookieName, depOptions.track);
     const disableConsentQueue = !!depOptions.disableConsentQueue;
-    // ToDo: cleanup TCF v1.1
-    const consentManagementProviderLegacy = new ConsentManagementProviderLegacy({
-        disableConsentQueue,
-        language: langManager.lang
-    });
     const consentManagementProvider = new ConsentManagementProvider({
         disableConsentQueue,
         language: langManager.lang
@@ -76,7 +70,7 @@ function initializeGDPR(options) {
     optInManager.setForcedStatusFromQueryParams(window.location.search);
 
     if (optInManager.checkCookieVersion()) {
-        consentManagementProviderLegacy.setVendorConsentCookie(null);
+        consentManagementProvider.setVendorConsentCookie(null);
     }
 
     const instance = new TrackingOptIn(
@@ -85,7 +79,6 @@ function initializeGDPR(options) {
         geoManager,
         contentManager,
         consentManagementProvider,
-        consentManagementProviderLegacy,
         {
             preventScrollOn,
             zIndex,
